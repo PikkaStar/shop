60e58bf5f0c7ddf6d958733a1aef2d49e13fda46
PikkaStar <yokotaka1226@gmail.com>
2023-11-13 13:38:05 +0000

    configure

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "6bc1517053da440bbf96ddbc36ba668d.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+
+  devise_for :customers,skip: [:passwords],controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+  resources :customers,only: [:show,:edit]
+  get 'check' => 'customers#check',as: "check"
+  devise_for :admins,skip: [:passwords,:registrations],controllers: {
+    sessions: "admin/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
